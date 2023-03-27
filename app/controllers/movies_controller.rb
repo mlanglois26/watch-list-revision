1eb845ae82deff7c48d8d7e0e4f8504ba5539897
marie langlois <marie.langlois2@gmail.com>
2023-03-27 16:21:25 +0200

    partie 1 ok

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,7 @@ class MoviesController < ApplicationController
   def about
   end
 
+  def index
+    @movies = Movie.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "about", to: "movies#about"
+    get "about", to: "movies#about"
+    get "index", to: "movies#index"
+
+    root to: "lists#index"
+    resources :lists, except: [:edit, :update] do
+      resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
